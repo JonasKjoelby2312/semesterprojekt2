2620a1ce6c4171ac60da5f06756c9316a39373b8
JonasKjoelby2312 <10503097@ucn.dk>
2024-05-07 12:28:09 +0200

    Employee

---
+++ src/db/EmployeeDB.java
@@ -0,0 +1,81 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Employee;
+import model.Product;
+
+public class EmployeeDB implements EmployeeDAO {
+	
+	private static final String FIND_ALL_Q = "select employee_id, name, company_position, salary from employee";
+	private static final String FIND_BY_ID_Q = FIND_ALL_Q + " where employee_id = ?";
+	private PreparedStatement findAllPS;
+	private PreparedStatement findByIdPS;
+	
+	public EmployeeDB() throws Exception {
+
+		Connection con = DBConnection.getInstance().getConnection();
+		try {
+			findAllPS = con.prepareStatement(FIND_ALL_Q);
+			findByIdPS = con.prepareStatement(FIND_BY_ID_Q);
+		} catch (SQLException e) {
+			throw new Exception("Could not prepare qurey", e);
+		}
+	}
+
+	@Override
+	public List<Employee> findAllEmployees() throws Exception {
+		ResultSet rs;
+		try {
+			rs = findAllPS.executeQuery();
+			List<Employee> res = buildObjects(rs);
+			return res;
+		} catch(SQLException e) {
+			throw new Exception("Could not retrive all employees", e);
+		}
+		
+	}
+
+	@Override
+	public Employee findEmployeeByID(int id) throws Exception {
+		Employee res = null;
+		ResultSet rs;
+		try {
+			findByIdPS.setInt(1, id);
+			rs = findByIdPS.executeQuery();
+			res = buildObject(rs);
+		} catch(SQLException e) {
+			throw new Exception("Could not find employee by ID", e);
+		}
+		return res;
+	}
+	
+	private Employee buildObject(ResultSet rs) throws SQLException {
+		Employee res = null;
+		if(rs.next()) {
+			res = new Employee(
+					rs.getString("name"),
+					rs.getString("company_position"),
+					rs.getInt("salary"),
+					rs.getInt("employee_no")
+					);
+		}
+		return res;
+	}
+	
+	private List<Employee> buildObjects(ResultSet rs) throws SQLException {
+		ArrayList<Employee> res = new ArrayList<>();
+		Employee e = buildObject(rs);
+		while (e != null) {
+			res.add(e);
+			e = buildObject(rs);
+		}
+		return res;
+	}
+}
+		
