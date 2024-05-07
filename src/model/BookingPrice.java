b24fc76883c581c59944ad245c29ed1d957fcdea
AugustPeterNielsen <150657560+AugustPeterNielsen@users.noreply.github.com>
2024-05-07 12:42:40 +0200

    Modellager BookingPrice

--- src/model/BookingPrice.java
+++ src/model/BookingPrice.java
@@ -1,5 +1,36 @@
 package model;
 
+import java.time.LocalDate;
+
 public class BookingPrice {
+	private LocalDate startDate;
+	private Double value;
+	
+	/*
+	 * Constructor for BookingPrice
+	 */
+	public BookingPrice(LocalDate startDate, Double value) {
+		this.startDate = startDate;
+		this.value = value;
+	}
+	
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+
+	public Double getValue() {
+		return value;
+	}
+
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
 
 }
