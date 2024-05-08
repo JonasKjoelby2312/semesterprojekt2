a2e91b81d0452ca873a050dca930cb159e25b0b6
AugustPeterNielsen <150657560+AugustPeterNielsen@users.noreply.github.com>
2024-05-08 09:56:27 +0200

    CustomerController

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,28 @@
 package controller;
 
-public class CustomerController {
+import java.util.List;
 
+import db.CustomerDAO;
+import db.CustomerDB;
+import model.Customer;
+
+/*
+ * CustomerController håndterer metoderne der ligger i CustomerDAO og retunere
+ * objecter fra databasen
+ */
+
+public class CustomerController {
+	private CustomerDAO customerDB;
+
+	public CustomerController() throws Exception {
+		customerDB = new CustomerDB();
+	}
+
+	public List<Customer> findAllCustomers() throws Exception {
+		return customerDB.findAllCustomers();
+	}
+
+	public Customer findCustomerByPhone(String phoneNo) throws Exception {
+		return customerDB.findCustomerByPhone(phoneNo);
+	}
 }
