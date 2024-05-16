5d78d1ebae661b678735894dd9ea3df75bf3c6be
ChristianBjerreSkadhauge <10503126@ucn.dk>
2024-05-16 10:13:45 +0200

    Customer Address fun + dogcut

--- src/db/CustomerDB.java
+++ src/db/CustomerDB.java
@@ -11,10 +11,14 @@ import model.Customer;
 
 public class CustomerDB implements CustomerDAO {
 	
-	private static final String FIND_ALL_Q = "select customer_id, name, email, a_id, phone_no, d_id from customer"; 
+	private static final String FIND_ALL_Q = "select customer_id, name, email, a_id, phone_no from customer";
+	private static final String FIND_CUSTOMER_ADDRESS_Q = "select address_id, house_no, road_name, zip from address right outer join zip_city on zip = zipcode";
 	private static final String FIND_BY_PHONE_Q = FIND_ALL_Q + " where phone_no = ?";
 	private static final String FIND_CUSTOMER_BY_ID_Q = FIND_ALL_Q + " where customer_id = ?";
+	
+	
 	private PreparedStatement findAllPS;
+	private PreparedStatement findCustomerAddressPS;
 	private PreparedStatement findByPhonePS;
 	private PreparedStatement findCustomerByID;
 	
@@ -22,6 +26,7 @@ public class CustomerDB implements CustomerDAO {
 		Connection con = DBConnection.getInstance().getConnection();
 		try {
 			findAllPS = con.prepareStatement(FIND_ALL_Q);
+			findCustomerAddressPS = con.prepareStatement(FIND_CUSTOMER_ADDRESS_Q);
 			findByPhonePS = con.prepareStatement(FIND_BY_PHONE_Q);
 			findCustomerByID = con.prepareStatement(FIND_CUSTOMER_BY_ID_Q);
 		} catch (SQLException e) {
@@ -39,6 +44,7 @@ public class CustomerDB implements CustomerDAO {
 		}
 		return res;
 	}
+	
 
 	@Override
 	public Customer findCustomerByPhone(String phoneNo) throws Exception {
@@ -69,11 +75,16 @@ public class CustomerDB implements CustomerDAO {
 				c = new Customer(
 						rs.getInt("customer_id"),
 						rs.getString("name"), 
-						"Bananstræde", //TODO
+						findAddress(),
 						rs.getString("email"),
 						rs.getString("phone_no"));
 		}
 		return c;
+	}
+
+	private String findAddress() {
+	
+		return null;
 	}
 
 	public Customer findCustomerByID(int id) throws Exception {
--- src/model/DogCut.java
+++ src/model/DogCut.java
@@ -1,5 +1,18 @@
 package model;
 
 public class DogCut {
+	private String comment;
+	
+	public DogCut(String comment) {
+		this.comment = comment;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 
 }
