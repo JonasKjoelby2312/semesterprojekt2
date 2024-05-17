8bae4478dc6b49d988a7b00524faa76164d53ecf
JonasKjoelby2312 <10503097@ucn.dk>
2024-05-17 11:27:52 +0200

    xxx

--- src/test/testFindAvailableTime.java
+++ src/test/testFindAvailableTime.java
@@ -13,15 +13,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import controller.OrderController;
+import db.BookingDB;
 import model.Booking;
 
 class testFindAvailableTime {
 	
 	private static OrderController orderCtrl;
+	private static BookingDB bookingDB;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		orderCtrl = new OrderController();
+		bookingDB = new BookingDB();
 	}
 
 	@AfterAll
@@ -37,8 +40,11 @@ class testFindAvailableTime {
 	}
 
 	@Test
-	void testFindAvailableTime(LocalDate Date, int employeeNo) {
-		
+	void testFindAvailableTime() throws Exception {
+		List<Booking> res = new ArrayList<>();
+		res = bookingDB.findAvailableTime(LocalDate.of(2024, 5, 16), 1);
+		System.out.println(res);
+		assertNotNull(res);
 	}
 
 }
