adc2df3d6fd4f285d269177c5d6171cd36d933ad
JonasKjoelby2312 <10503097@ucn.dk>
2024-05-22 15:09:47 +0200

    Update testCreateBookingWithTime.java

--- src/test/testCreateBookingWithTime.java
+++ src/test/testCreateBookingWithTime.java
@@ -2,16 +2,24 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import controller.OrderController;
+
 class testCreateBookingWithTime {
+	
+	private static OrderController oc; 
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		oc = new OrderController();
 	}
 
 	@AfterAll
@@ -27,8 +35,12 @@ class testCreateBookingWithTime {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCreateBookingPersonWithTimeAndDate() throws Exception {
+		boolean currBooking = oc.createBookingPerson(1, 1, "51938113", LocalDate.of(2024, 5, 25), LocalTime.of(15, 30));
+		assertNotNull(currBooking);
+		boolean res = oc.completeBooking();
+		System.out.println(assertFalse(res));
+		
 	}
 
 }
