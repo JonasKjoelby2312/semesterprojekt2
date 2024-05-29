8a8d198dd666ccc1b2335dfae0d73dbe064502e5
Beslag1c <10503120@ucn.dk>
2024-05-29 09:29:11 +0200

    JavaDOC

--- src/db/BookingTypeDAO.java
+++ src/db/BookingTypeDAO.java
@@ -5,24 +5,25 @@ import java.util.List;
 import model.BookingType;
 
 /**
- * A interface for accessing booking type data
- * Provides method to get all booking types and a specific booking type by its ID
+ * A interface for accessing booking type data Provides method to get all
+ * booking types and a specific booking type by its ID
  */
 public interface BookingTypeDAO {
-	
+
 	/**
 	 * Returns all booking types from the database
+	 * 
 	 * @return a list of booking types
-	 * @throws Exception if a database access error appear
+	 * @throws DataAccessException if a database access error appear
 	 */
-	List<BookingType>findAllBookingTypes() throws DataAccessException;
-	
+	List<BookingType> findAllBookingTypes() throws DataAccessException;
+
 	/**
 	 * returns a specific booking type by its ID from the database.
 	 * 
 	 * @param bookingTypeID is the id of the booking type to return
 	 * @return The BookingType object that matches the given ID, or null if not found
-	 * @throws Exception if a database access error appear
+	 * @throws DataAccessException if a database access error appear
 	 */
 	BookingType findBookingType(int bookingTypeID) throws DataAccessException;
 
--- src/db/CustomerDAO.java
+++ src/db/CustomerDAO.java
@@ -4,8 +4,27 @@ import java.util.List;
 
 import model.Customer;
 
+/**
+ * Interface for accessing booking price data Provides method to get all
+ * customers based on their phone no
+ */
+
 public interface CustomerDAO {
+
 	
+	/**
+	 * Returns all customers from the database
+	 * @return a list of customers
+	 * @throws DataAccessException if a database access error appear
+	 */
 	List<Customer> findAllCustomers() throws DataAccessException;
+	
+	/**
+	 * returns a specific customer by its phoneNO from the database.
+	 * 
+	 * @param phoneNo is the phone number of the customer to return
+	 * @return The CustomerByPhone object that matches the given phoneNo, or null if not found
+	 * @throws DataAccessException if a database access error appear
+	 */
 	Customer findCustomerByPhone(String phoneNo) throws DataAccessException;
 }
